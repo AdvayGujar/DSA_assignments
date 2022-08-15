6529f9d8bfe3d2a32c47f26c6a7c781c232e5ba8
advay2003 <advay2003gujar@gmail.com>
2022-08-15 17:18:54 +0530

    Corrected the program to find from entire array of 20 elements

--- largest.c
+++ largest.c
@@ -4,12 +4,12 @@ int main()
 	int arr[20],i,largest=0;
 
 	printf("\nEnter the elements of the array.\n");
-	for(i=0;i<10;i++)
+	for(i=0;i<20;i++)
 	{
 		scanf("%d",& arr[i]);
 	}
 
-	for(i=0;i<10;i++)
+	for(i=0;i<20;i++)
 	{
 		if(arr[i] > largest)
 			largest = arr[i];
